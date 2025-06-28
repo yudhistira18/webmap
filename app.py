822dfaf4861640e045f6a2be2fae75f0e5cbf5ae
yudhistira18 <yudhistiraaudley@gmail.com>
2025-06-28 14:39:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import folium
 from streamlit_folium import st_folium
 from io import BytesIO
 import plotly.express as px
-
+import plotly.graph_objects as go
 st.set_page_config(layout="wide")
 st.title("🗂️ Composite Data Bor")
 
