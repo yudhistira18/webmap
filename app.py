3093d5d3c66d25b465373989599245281289f0a3
yudhistira18 <yudhistiraaudley@gmail.com>
2025-06-28 09:31:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,17 @@ m = folium.Map(location=[gdf.geometry.y.mean(), gdf.geometry.x.mean()], zoom_sta
 
 # Tambahkan titik ke peta
 for _, row in gdf.iterrows():
-    popup = f"BHID: {row['BHID']}<br>Layer: {row['Layer']}<br>Ni
+    popup = (
+        f"<b>BHID:</b> {row['BHID']}<br>"
+        f"<b>Layer:</b> {row['Layer']}<br>"
+        f"<b>Ni:</b> {row['Ni']:.2f}<br>"
+    )
     folium.CircleMarker(
         location=[row.geometry.y, row.geometry.x],
         radius=6,
         color='red',
         fill=True,
+        fill_opacity=0.8,
         popup=popup
     ).add_to(m)
 
